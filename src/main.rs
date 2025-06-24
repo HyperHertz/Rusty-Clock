89a8a93a8826aa450b8d5d5de287212c36b1e561
HyperHertz <cbcurry94@gmail.com>
2025-06-24 16:59:20 -0500

    more themes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#![cfg_attr(
+    all(not(debug_assertions), any(target_os = "windows", target_os = "linux")),
+    windows_subsystem = "windows"
+)]
 use chrono::Local;
 use eframe::egui;
 use eframe::App as EframeApp;
@@ -6,14 +10,59 @@ struct ClockApp;
 
 impl EframeApp for ClockApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        //TO-DO, add multiple themes
-        // ctx.set_visuals(egui::Visuals::light());
-        // ctx.set_visuals(egui::Visuals::dark());
+        // Timer variable, centering, and themes
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let time = Local::now().format("%I:%M:%S %p\n%b %e %a, %Y").to_string();
+            ui.vertical_centered(|ui| {
+                //ui.label(&time);
+                ui.heading(&time);
 
-        let mut visuals = egui::Visuals::dark();
-        visuals.override_text_color = Some(egui::Color32::from_rgb(51, 255, 0));
-        visuals.panel_fill = egui::Color32::from_rgb(75, 0, 130);
-        ctx.set_visuals(visuals);
+                // TO-DO, makes buttons, probably use this to let user change themes
+                ui.label("Themes:");
+                if ui
+                    .add(egui::Button::new("Green and Purple").corner_radius(12.0))
+                    .clicked()
+                {
+                    let mut visuals = egui::Visuals::dark();
+                    visuals.override_text_color = Some(egui::Color32::from_rgb(51, 255, 0));
+                    visuals.panel_fill = egui::Color32::from_rgb(75, 0, 130);
+                    ctx.set_visuals(visuals);
+                }
+                if ui
+                    .add(egui::Button::new("Light Theme").corner_radius(12.0))
+                    .clicked()
+                {
+                    let visuals = egui::Visuals::light();
+                    ctx.set_visuals(visuals);
+                }
+
+                if ui
+                    .add(egui::Button::new("Dark Theme").corner_radius(12.0))
+                    .clicked()
+                {
+                    let visuals = egui::Visuals::dark();
+                    ctx.set_visuals(visuals);
+                }
+                if ui
+                    .add(egui::Button::new("Dark Red").corner_radius(12.0))
+                    .clicked()
+                {
+                    let mut visuals = egui::Visuals::dark();
+                    visuals.override_text_color = Some(egui::Color32::from_rgb(255, 0, 0));
+                    visuals.panel_fill = egui::Color32::from_rgb(51, 51, 51);
+                    ctx.set_visuals(visuals);
+                }
+                if ui
+                    .add(egui::Button::new("Dark Green").corner_radius(12.0))
+                    .clicked()
+                {
+                    let mut visuals = egui::Visuals::dark();
+                    visuals.override_text_color = Some(egui::Color32::from_rgb(51, 255, 0));
+                    visuals.panel_fill = egui::Color32::from_rgb(51, 51, 51);
+                    ctx.set_visuals(visuals);
+                }
+            });
+        });
 
         // // TO-DO, figure out the rounded corners modules
         // let mut style = (*ctx.style()).clone();
@@ -40,23 +89,6 @@ impl EframeApp for ClockApp {
         // TO-DO, play with DPI scaling (make UI bigger/smaller)
         //ctx.set_pixels_per_point(1.0); // 1.0 is normal, >1.0 is bigger
 
-        // Timer variable and Centering
-        egui::CentralPanel::default().show(ctx, |ui| {
-            let time = Local::now().format("%I:%M:%S %p\n%b %e %a, %Y").to_string();
-            ui.vertical_centered(|ui| {
-                //ui.label(&time);
-                ui.heading(&time);
-            });
-
-            // TO-DO, makes buttons, probably use this to let user change themes
-            // ui.colored_label(egui::Color32::LIGHT_BLUE, "Welcome to your custom clock!");
-            // if ui
-            //     .add(egui::Button::new("A round button!").rounding(12.0))
-            //     .clicked()
-            // {
-            //     // Button pressed!
-            // }
-        });
         ctx.request_repaint_after(std::time::Duration::from_millis(200));
     }
 }
@@ -65,7 +97,7 @@ fn main() -> eframe::Result<()> {
     // Window
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([260.0, 150.0])
+            .with_inner_size([250.0, 300.0])
             .with_title("Rusty Clock"),
         ..Default::default()
     };
